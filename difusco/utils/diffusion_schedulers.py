6aec89a11c79dc25c642eb1e9041a75ac20d1416
Daniyar Zakarin <daniyarzt@gmail.com>
2024-04-19 16:31:27 +0200

    Added multiple categories to CategoricalDiffusion. Didn't test much.

--- difusco/utils/diffusion_schedulers.py
+++ difusco/utils/diffusion_schedulers.py
@@ -46,9 +46,10 @@ class GaussianDiffusion(object):
 class CategoricalDiffusion(object):
   """Gaussian Diffusion process with linear beta scheduling"""
 
-  def __init__(self, T, schedule):
+  def __init__(self, T, schedule, K):
     # Diffusion steps
     self.T = T
+    self.K = K
 
     # Noise schedule
     if schedule == 'linear':
@@ -61,12 +62,12 @@ class CategoricalDiffusion(object):
       self.beta = np.clip(1 - (self.alphabar[1:] / self.alphabar[:-1]), None, 0.999)
 
     beta = self.beta.reshape((-1, 1, 1))
-    eye = np.eye(2).reshape((1, 2, 2))
-    ones = np.ones((2, 2)).reshape((1, 2, 2))
+    eye = np.eye(self.K).reshape((1, self.K, self.K))
+    ones = np.ones((self.K, self.K)).reshape((1, self.K, self.K))
 
     self.Qs = (1 - beta) * eye + (beta / 2) * ones
 
-    Q_bar = [np.eye(2)]
+    Q_bar = [np.eye(self.K)]
     for Q in self.Qs:
       Q_bar.append(Q_bar[-1] @ Q)
     self.Q_bar = np.stack(Q_bar, axis=0)
@@ -78,9 +79,8 @@ class CategoricalDiffusion(object):
   def sample(self, x0_onehot, t):
     # Select noise scales
     Q_bar = torch.from_numpy(self.Q_bar[t]).float().to(x0_onehot.device)
-    xt = torch.matmul(x0_onehot, Q_bar.reshape((Q_bar.shape[0], 1, 2, 2)))
-    return torch.bernoulli(xt[..., 1].clamp(0, 1))
-
+    xt = torch.matmul(x0_onehot, Q_bar) # There used to be a weird reshape for Q_bar. 
+    return torch.multinomial(xt.clamp(0, 1).reshape(x0_onehot.shape), 1)
 
 class InferenceSchedule(object):
   def __init__(self, inference_schedule="linear", T=1000, inference_T=1000):
